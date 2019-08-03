9fea00966404bc7271e5e34b1bbdfa914a9432ad
gitandlucsil <andlucsil1221@gmail.com>
2019-08-02 22:33:56 -0300

    criado metodo para ligar e desligar leds

--- esp8266_mqtt/esp8266_mqtt.ino
+++ esp8266_mqtt/esp8266_mqtt.ino
@@ -4,7 +4,8 @@
 
 const char* ssid = "";
 const char* senha = "";
-const byte LED = 2;
+const byte LED_1 = 2;
+const byte LED_2 = 16;
 
 WiFiServer server(80);
 
@@ -32,6 +33,8 @@ bool conectaWiFi(void)
 }
 void setup() 
 {
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
   Serial.begin(115200);
   delay(10);
   if(conectaWiFi() == true){
@@ -39,7 +42,8 @@ void setup()
     Serial.println("Servidor inicializado");
   }
 }
-int cont = 0;
+boolean statusLED_1 = LOW;
+boolean statusLED_2 = LOW;
 void loop() 
 {
   WiFiClient client = server.available();
@@ -50,15 +54,58 @@ void loop()
     delay(1);
   }
   Serial.println("Cliente conectado!");
+
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  if(req.indexOf("LED1on") != -1){
+    digitalWrite(LED_1,LOW);
+    statusLED_1 = HIGH;
+  }else if(req.indexOf("LED1off") != -1){
+    digitalWrite(LED_1,HIGH);
+    statusLED_1 = LOW;
+  }
+  if(req.indexOf("LED2on") != -1){
+    digitalWrite(LED_2,LOW);
+    statusLED_2 = HIGH;
+  }else if(req.indexOf("LED2off") != -1){
+    digitalWrite(LED_2,HIGH);
+    statusLED_2 = LOW;
+  }
+
+  if(req.indexOf("LEDTodoson") != -1){
+    digitalWrite(LED_1,LOW);
+    digitalWrite(LED_2,LOW);
+    statusLED_1 = HIGH;
+    statusLED_2 = HIGH;
+  }else if(req.indexOf("LEDTodosoff") != -1){
+    digitalWrite(LED_1,HIGH);
+    digitalWrite(LED_2,HIGH);
+    statusLED_1 = LOW;
+    statusLED_2 = LOW;
+  }
+  
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("");
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
-  client.println("<meta http-equiv='refresh' content='2'>");
+  //client.println("<meta http-equiv='refresh' content='2'>");
   client.println("<h1>Curso controle dispositivos remotamente com ESP8266</h1>");
   client.println("<h2>Ola Makers!</h2>");
-  client.println(cont);
+  if(!statusLED_1){
+    client.println("<p>LED 1 DESLIGADO <a href=\"LED1on\"><button>LIGAR</button></a></p>"); 
+  }else{
+    client.println("<p>LED 1 LIGADO <a href=\"LED1off\"><button>DESLIGAR</button></a></p>"); 
+  }
+  if(!statusLED_2){
+    client.println("<p>LED 2 DESLIGADO <a href=\"LED2on\"><button>LIGAR</button></a></p>"); 
+  }else{
+    client.println("<p>LED 2 LIGADO <a href=\"LED2off\"><button>DESLIGAR</button></a></p>"); 
+  }
+  client.println("<p><a href=\"LEDTodoson\"><button>LIGAR TODOS</button></a></p>"); 
+  client.println("<p><a href=\"LEDTodosoff\"><button>DESLIGAR TODOS</button></a></p>"); 
+
   client.println("</html>"); 
-  cont++;
 }
