fc18b5628973de83bc9bcae186059b3ec57c65ba
gitandlucsil <andlucsil1221@gmail.com>
2019-08-02 21:02:49 -0300

    acendendo os dois leds simultaneamente

--- esp8266_mqtt/esp8266_mqtt.ino
+++ esp8266_mqtt/esp8266_mqtt.ino
@@ -1,14 +1,17 @@
-const byte LED = 2; //Pino do LED do NodeMCU
-
+const byte LED_1 = 2; //Pino do LED do NodeMCU
+const byte LED_2 = 16; //Pino do LED do NodeMCU
 void setup() 
 {
-  pinMode(LED,OUTPUT);
+  pinMode(LED_1,OUTPUT);
+  pinMode(LED_2,OUTPUT);
 }
 
 void loop() 
 {
-  digitalWrite(LED,LOW);//Acender o LED
+  digitalWrite(LED_1,LOW);//Acender o LED
+  digitalWrite(LED_2,HIGH);//Acender o LED
   delay(1000);
-  digitalWrite(LED,HIGH);//Apagar o LED
+  digitalWrite(LED_1,HIGH);//Apagar o LED
+  digitalWrite(LED_2,LOW);//Acender o LED
   delay(1000);
 }
